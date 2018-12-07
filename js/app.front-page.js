870ca53102c4d5186e19db0681cc1995412c2423
Dani Nugraha <dani.hmng@gmail.com>
2018-12-07 23:41:34 +0700

    pop up headline

--- js/app.front-page.js
+++ js/app.front-page.js
@@ -12,4 +12,25 @@
         autoplaySpeed: 10000
     });
 
+    $('.trigger-image-modal').each(function (index, i) {
+        var img = $(i).find('img');
+        var modal = $('#'+$(i).data('target'));
+        var modalImg = modal.find('img');
+        img.click(function () {
+            modal.show();
+            modalImg.prop('src', img.prop('src'));
+        });
+
+        modal.click(function (event) {
+            if($(event.target).prop('id') == modal.prop('id')) {
+                modal.hide();
+            }
+        });
+
+        var span = $(modal.find(".close")[0]);
+        span.click(function () {
+            modal.hide();
+        });
+    })
+
 })(jQuery); // End of use strict
